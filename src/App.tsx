443a4e76e5dcb11f0f2828c91a75f16f85480611
Kenneth Ofosu <kennethofosu01@gmail.com>
2025-05-08 13:22:36 -0400

    setup typescript, simplify homepage

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div style={{ padding: "2rem", fontFamily: "system-ui" }}>
+      <h1>Hello, I'm Kenneth</h1>
+      <p>Welcome to my personal site. This is under construction.</p>
+    </div>
+  );
+}
+
+export default App
