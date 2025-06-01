16dd058eca774bd0decd5e4985ebe16097e2166f
Kenneth Ofosu <kennethofosu01@gmail.com>
2025-06-01 02:11:29 -0400

    feat(layout): add theme toggle to Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,11 @@
 import { header } from "./Header.css";
+import { ThemeToggle } from "../ThemeToggle/ThemeToggle";
 
 export function Header() {
     return (
-        <header className={header}>
-            <h1>Kenneth Ofosu</h1>
-        </header>
+      <header className={header}>
+        <h1>Kenneth Ofosu</h1>
+        <ThemeToggle />
+      </header>
     )
 }
